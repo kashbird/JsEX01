4e04882ec72fe1292af6d100c78bf0c5b7e2ebb6
kashbird <78414807+kashbird@users.noreply.github.com>
2022-03-15 15:45:44 +0900

    haha

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-const myHead = document.querySelector('h1');
-myHead.textContent = 'Hello World 첫번째 만드는 자바스크립트';
+// const myHead = document.querySelector('h1');
+// myHead.textContent = 'Hello World 첫번째 만드는 자바스크립트';
+
+// document.body.innerHTML ='<h2>안녕하세요</h2>';
+
+const btnBg = document.querySelector('.btn_bg');
+btnBg.addEventListener('click', function () {
+    document.body.classList.toggle('changeBlue');
+});
