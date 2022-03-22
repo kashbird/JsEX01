b8c77a9a4d3a09d579d2323ac9d7f9801010d15f
kashbird <artisthg122@gmail.com>
2022-03-22 16:25:33 +0900

    gkgkgkgkg

--- main.js
+++ main.js
@@ -3,17 +3,40 @@
 
 // document.body.innerHTML ='<h2>안녕하세요</h2>';
 
+const clr = ['changeBlue', 'changeRed', 'changePurple', 'changeImg']
+const active = ['activeBlue', 'activeRed'];
+
 const blueBtn = document.querySelector('.btn_blue');
-blueBtn.addEventListener('click', function () {
-    document.body.classList.toggle('changeBlue');
-});
+blueBtn.addEventListener('click', function() {
+    document.body.classList.remove(...clr);
+    document.body.classList.add('changeBlue');
+    blueBtn.classList.add('activeBlue');   
+})
 
 const redBtn = document.querySelector('.btn_red');
 redBtn.addEventListener('click', function () {
-    document.body.classList.toggle('changeRed');
+    document.body.classList.remove(...clr);
+    document.body.classList.add('changeRed');
+    redBtn.classList.add('activeRed');
 });
 
 const purpleBtn = document.querySelector('.btn_purple');
 purpleBtn.addEventListener('click', function () {
-    document.body.classList.toggle('changePurple');
+    document.body.classList.remove(...clr);
+    document.body.classList.add('changePurple');
+    purpleBtn.classList.add('activepurple');
 });
+
+const imgBtn = document.querySelector('.btn_img');
+const btns = document.querySelector('.bg_img');
+imgBtn.addEventListener('click', function () {
+    document.body.classList.remove(...clr);
+    btns.classList.toggle('bg_img--show');
+    imgBtn.classList.add('activeImg');
+});
+
+const resetBtn = document.querySelector('.btn_reset');
+resetBtn.addEventListener('click', function() {
+    document.body.classList.remove(...clr);
+});
+
